95c075fc8e0041ae35f0f66180a233b47d54ed4b
Rémi Cossette-Roberge <remi.cossette-roberge@usherbrooke.ca>
2017-12-04 15:00:36 -0500

    allo

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,90 @@
+#include "pch.h"
+
+#include "../utils.h"
+
+using vecf = std::vector<float>;
+using veci = std::vector<int>;
+
+TEST(EQUAL) {
+	vecf v1(10, 0.f);
+	vecf v2(10, 0.f);
+	vecf v3(10, 1.f);
+	vecf v4(5, 0.f);
+	EXPECT_TRUE(equals(v1, v2));
+	EXPECT_TRUE(equals(v2, v1));
+	EXPECT_FALSE(equals(v1, v3));
+	EXPECT_FALSE(equals(v1, v4));
+}
+
+TEST(ADD) {
+	vecf v1(5, 1.f);
+	vecf v2(5, 2.f);
+	vecf expect(5, 3.f);
+	vecf& res = add(v1, v2);
+	EXPECT_TRUE(equals(res, expect));
+	EXPECT_TRUE(equals(res, v1));
+	EXPECT_FALSE(equals(res, v2));
+	EXPECT_TRUE(&v1 == &res);
+}
+
+TEST(MULT_SCALAR) {
+	vecf v1(5, 2.f);
+	float f = 3.f;
+	vecf expect(5, 6.f);
+	auto& res = mult(v1, f);
+	EXPECT_TRUE(equals(res, expect));
+	EXPECT_TRUE(equals(res, v1));
+	EXPECT_TRUE(&v1 == &res);
+}
+
+
+		TEST(MULT_VEC)
+		{
+			vecf v1(5, 2.f);
+			vecf v2(5, 3.f);
+			vecf expect(5, 6.f);
+			vecf res = mult(v1, v2);
+			EXPECT_TRUE(equals(res, expect));
+			EXPECT_FALSE(equals(res, v1));
+			EXPECT_FALSE(equals(res, v2));
+		}
+		TEST(SUM)
+		{
+			vecf v1(5, 2.f);
+			float expect = 10.f;
+			float res = sum(v1);
+			EXPECT_TRUE(epsilon(res - expect));
+		}
+		TEST(LENGTH)
+		{
+			vecf v1{ 3,4 };
+			vecf v2(16, 4.f);
+			float expect1 = 5.f;
+			float expect2 = 16.f;
+			float res1 = length(v1);
+			float res2 = length(v2);
+			EXPECT_TRUE(epsilon(expect1 - res1));
+			EXPECT_TRUE(epsilon(expect2 - res2));
+		}
+		TEST(AVERAGE)
+		{
+			veci indexes{ 0,2 };
+			vector<vecf> data{ vecf(5, -1.f), vecf(5, -100.f), vecf(5, 1.f) };
+			vecf expect(5, 0.f);
+			vecf res = average(indexes, data);
+			EXPECT_TRUE(equals(expect, res));
+		}
+		TEST(VARIANCE)
+		{
+			veci indexes{ 0,2 };
+			vector<vecf> data{ vecf(5, -1.f), vecf(5, -100.f), vecf(5, 1.f) };
+			vecf avg = average(indexes, data);
+			vecf res = variance(indexes, data, avg);
+			vecf expect(5, 1.f);
+			EXPECT_TRUE(equals(expect, res));
+		}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
